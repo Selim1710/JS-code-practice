7b5116ace5e3c067bafe782044822e18aca90667
Md. Selim hossain suhag <you@example.com>
2022-07-21 17:39:30 +0600

    update nav slider

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-////////////////////////     navbar       ////////////////////////\
+////////////////////////     navbar      ////////////////////////
+let marker = document.querySelector(".marker");
+let item = document.querySelectorAll("nav a");
 
+function indicator(e) {
+  marker.style.left = e.offsetLeft + "px";
+  marker.style.width = e.offsetWidth + "px";
+}
 
+item.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    indicator(e.target);
+  });
+});
+
+////////////////////////     end-navbar      ////////////////////////
 
 // arrar short()
 let elements = ['bananna', 'apple', 'cat', 'mouse', 'cannon'];
