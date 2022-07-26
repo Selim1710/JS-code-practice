4ba17b1e0c025a4f04630ce91dbbecfb3f86e6a8
Md. Selim hossain suhag <you@example.com>
2022-07-26 16:39:31 +0600

    notice 2

--- main.js
+++ main.js
@@ -13,6 +13,39 @@ item.forEach((link) => {
   });
 });
 
+////////////////////////     notice-2      ////////////////////////
+var p = $('.par');
+var animator = $('<span class="anim"></span>');
+var hidden = $('<span class="hid"></span>')
+var textLength = p.text().length;
+var offset = 0;
+
+function animate(letter_position) {
+    var text = p.text();
+    p.text('');
+    var textBefore = text.substring(0, letter_position - 1);
+    var textAfter = text.substring(letter_position, textLength);
+    var letter = text.substr(letter_position - 1, 1);
+    animator.text(letter);
+    hidden.text(textAfter);
+    p.append(textBefore);
+    p.append(animator);
+    p.append(hidden);
+}
+
+var interval = setInterval(function(){
+    if (p.hasClass('hid')) p.removeClass('hid');
+    if (offset - 2 === textLength) {
+        clearInterval(interval);
+        p.children('.anim').remove();
+        p.children('.hid').remove();
+        return;
+    } 
+    animate(offset);
+    offset++;
+}, 100);
+
+
 ////////////////////////     Animated Smoke Test      ////////////////////////
 window.onload = function () {
   // split text into letters
